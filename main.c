20d80e497605b5a3f11994d806b5593b09258047
PedroUlian <83662297+PedroUlian@users.noreply.github.com>
2026-05-04 14:50:48 -0300

    Implement interactive calculator mode
    Added interactive mode for calculator when no arguments are provided.

--- main.c
+++ main.c
@@ -2,16 +2,42 @@
 #include "lexer.h"
 #include "parser.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-	init_lexer(argv[1]);
-	init_parser();
+    if(argc > 1){
+        init_lexer(argv[1]);
+        init_parser();
 
-	Node *tree = parse_expression();
-	double result = evaluate(tree);
-	
-	printf("%f\n", result);
+        Node *tree = parse_expression();
+        double result = evaluate(tree);
 
-	return 0;
+        printf("%f\n", result);
+    }
+    else{
+        char input[256];
+
+        while(1){
+            printf("calc> ");
+
+            if(!fgets(input, sizeof(input), stdin))
+                break;
+
+            input[strcspn(input, "\n")] = 0;
+
+            if(strcmp(input, "exit") == 0)
+                break;
+
+            init_lexer(input);
+            init_parser();
+
+            Node *tree = parse_expression();
+            double result = evaluate(tree);
+
+            printf("%f\n", result);
+        }
+    }
+
+    return 0;
 }
